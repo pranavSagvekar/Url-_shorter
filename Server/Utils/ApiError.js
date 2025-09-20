2374dbffebf961d65d8c29bd7d25ccf1aec0b77a
pranavSagvekar <pranavsagvekar78@gmail.com>
2025-09-20 22:29:59 +0530

    Add error handling utilities: ThrowError, ApiResponce, and AsyncHandler classes

---
+++ Server/Utils/ApiError.js
@@ -0,0 +1,10 @@
+class ThrowError extends Error {
+    constructor(msg = 'Something went wrong', statusCode = 400, data = {}) {
+        super(msg);
+        this.statusCode = statusCode;
+        this.data = data;
+    }
+
+}
+
+export {ThrowError};

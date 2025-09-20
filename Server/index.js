0bfb1991dd75488a42961caba9cb9fd67d0713be
pranavSagvekar <pranavsagvekar78@gmail.com>
2025-09-20 22:04:58 +0530

    Refactor database connection setup in index.js

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,13 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { APP_CONSTRAIN } from "./config/constant.js";
-
 import app from "./server.js";
+import connectDB from "./DB/connect.js";
 
 dotenv.config();
 
-dotenv.config(
-    
-)
+
+  connectDB();
 
 
 app.listen( APP_CONSTRAIN.PORT  || 3000 ,() => {

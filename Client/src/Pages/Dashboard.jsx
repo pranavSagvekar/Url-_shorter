d92506fc3ac2c4852f03e70cefd14d3864dc9fa2
pranavSagvekar <pranavsagvekar78@gmail.com>
2025-10-02 18:21:04 +0530

    frontend corrected

--- Client/src/Pages/Dashboard.jsx
+++ Client/src/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import useAuth from "../hooks/useAuth";
+import useAuth from "../Hooks/useAuth.js";
 import useUrl from "../Hooks/useUrl";
 import { Link, ChartColumn, Plus, Search } from "lucide-react";
 import { motion } from "framer-motion";
